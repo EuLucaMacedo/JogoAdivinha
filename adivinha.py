9d1aef0180705331fd041026153f8f35c5d15983
Luca Macedo <lucamacedo.lm@gmail.com>
2025-07-03 01:37:20 -0300

    Refatorando algumas melhorias

--- adivinha.py
+++ adivinha.py
@@ -4,24 +4,31 @@ def jogo_adivinhacao():
     numero_sorteado = random.randint(0, 10)
     tentativas = 0
 
+    print('🎯 Bem-vindo ao jogo: Adivinhe o Número!')
     print('🤖 Pensei em um número de 0 a 10. Consegue adivinhar?')
 
     while True:
         try:
             chute = int(input('Digite seu palpite: '))
+            tentativas += 1
+
         except ValueError:
             print('❌ Entrada inválida. Digite um número inteiro entre 0 e 10.')
             continue
 
-        tentativas += 1
-
+        if chute < numero_sorteado:
+            print('🔻 Muito baixo.')
+        elif chute > numero_sorteado:
+            print("🔺 Muito alto.")
         if chute != numero_sorteado:
             print(f'Você escolheu {chute}, ERROUUU! 😅 Tente novamente...\n')
         else:
-            print(f'🎯 Acertou! O número era {numero_sorteado}.')
-            print(f'Você precisou de {tentativas} tentativa(s).')
             break
 
+    print(f'🎯 Acertou! O número era {numero_sorteado}.')
+    print(f'Você precisou de {tentativas} tentativa(s).')
     print('🏁 Fim do Jogo!')
 
 
+if __name__ == "__main__":
+    jogo_adivinhacao()
